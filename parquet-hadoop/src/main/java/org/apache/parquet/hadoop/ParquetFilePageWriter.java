b95be40c00aaf41cfac5824cb934bd6b89c3c352
Nick Murphy <njmurphy@gmail.com>
2016-05-24 14:49:27 -0500

    Write pages from given blocks rather than chunks

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFilePageWriter.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFilePageWriter.java
@@ -8,10 +8,13 @@ import org.apache.parquet.column.page.PageWriteStore;
 import org.apache.parquet.column.page.PageWriter;
 import org.apache.parquet.hadoop.ParquetFileReader.Chunk;
 import org.apache.parquet.hadoop.ParquetFileReader.ChunkPageSet;
+import org.apache.parquet.hadoop.metadata.BlockMetaData;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.parquet.schema.MessageType;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -38,50 +41,70 @@ class ParquetFilePageWriter {
   /**
    * Writes raw pages from the given column chunks.
    *
-   * @param fileWriter File writer to which to append
-   * @param chunks Column chunks to append, which must not contain dictionary
-   *               pages
+   * @param fileWriter File writer to which to write pages
+   * @param fileReaders File readers from which to read chunks for blocks
+   * @param blockLists Blocks for each file whose pages to append, which must
+   *                   not make use of dictionary pages
    * @throws IllegalArgumentException if any column chunks include dictionary
    *                                  pages
    */
   void writePages(ParquetFileWriter fileWriter,
-                  Iterable<Chunk> chunks) throws IOException {
+                  Iterable<ParquetFileReader> fileReaders,
+                  Iterable<List<BlockMetaData>> blockLists)
+      throws IOException {
     CodecFactory.BytesCompressor compressor =
         codecFactory.getCompressor(codecName, pageSize);
     ColumnChunkPageWriteStore pageWriteStore =
         new ColumnChunkPageWriteStore(compressor, schema, pageSize);
+    Map<ColumnDescriptor, CopyPageVisitor> copyVisitors
+        = newCopyVisitors(pageWriteStore);
 
-    long bufferedSize = 0L;
-    Map<ColumnDescriptor, CopyPageVisitor> copyVisitors =
-        newCopyVisitors(pageWriteStore);
+    final Iterator<List<BlockMetaData>> blockListsIterator
+        = blockLists.iterator();
+    long bufferedSize = 0L, rowCount = 0L;
 
-    for (Chunk chunk : chunks) {
-      ChunkPageSet pageSet = chunk.readRawPages();
-      if (pageSet.hasDictionaryPage())
-        throw new IllegalArgumentException("Chunks may not have dictionary pages");
+    for (ParquetFileReader fileReader : fileReaders) {
+      List<BlockMetaData> blocks = blockListsIterator.next();
 
-      final ColumnDescriptor column = chunk.getColumnDescriptor();
-      final CopyPageVisitor copyVisitor = copyVisitors.get(column);
-
-      for (DataPage dataPage : pageSet.getDataPages()) {
-        final int uncompressedSize = dataPage.getUncompressedSize();
-        if (bufferedSize + uncompressedSize > rowGroupSize) {
-          pageWriteStore.flushToFileWriter(fileWriter);
-          copyVisitors = newCopyVisitors(pageWriteStore);
-          bufferedSize = 0L;
+      for (BlockMetaData block : blocks) {
+        final long blockSize = block.getTotalByteSize();
+        if (bufferedSize + blockSize > rowGroupSize) {
+          flush(fileWriter, rowCount, pageWriteStore);
+          bufferedSize = rowCount = 0L;
         }
 
-        try {
-          dataPage.accept(copyVisitor);
-        } catch (CopyPageVisitor.PageWriteException e) {
-          throw (IOException)e.getCause();
+        final List<Chunk> chunks = fileReader.readChunks(block);
+        for (Chunk chunk : chunks) {
+          ChunkPageSet pageSet = chunk.readRawPages();
+          if (pageSet.hasDictionaryPage())
+            throw new IllegalArgumentException("Chunks may not have dictionary pages");
+
+          final ColumnDescriptor column = chunk.getColumnDescriptor();
+          final CopyPageVisitor copyVisitor = copyVisitors.get(column);
+
+          for (DataPage dataPage : pageSet.getDataPages()) {
+            try {
+              dataPage.accept(copyVisitor);
+            } catch (CopyPageVisitor.PageWriteException e) {
+              throw (IOException)e.getCause();
+            }
+          }
         }
-        bufferedSize += uncompressedSize;
+
+        bufferedSize += blockSize;
+        rowCount += block.getRowCount();
       }
     }
 
     if (bufferedSize > 0L)
-      pageWriteStore.flushToFileWriter(fileWriter);
+      flush(fileWriter, rowCount, pageWriteStore);
+  }
+
+  private void flush(ParquetFileWriter fileWriter, long recordCount,
+                     ColumnChunkPageWriteStore pageWriteStore) throws IOException {
+    fileWriter.startBlock(recordCount);
+    pageWriteStore.flushToFileWriter(fileWriter);
+    fileWriter.endBlock();
   }
 
   private Map<ColumnDescriptor, CopyPageVisitor> newCopyVisitors(
